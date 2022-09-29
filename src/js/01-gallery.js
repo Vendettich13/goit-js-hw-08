aba21ce12b6fbb9a399037f6ad09824b04d2b46a
Victor Litvynchuk <vendettich13@icloud.com>
2022-09-29 23:37:02 +0300

    try to send hw

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,30 @@
 // Add imports above this line
+import galleryTpls from '../js/templates/gallery.hbs';
+import gallery from './gallery.json';
 import SimpleLightbox from 'simplelightbox';
-import { galleryItems } from './gallery-items';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+const refs = {
+  galleryList: document.querySelector('.gallery'),
+};
 
-console.log(galleryItems);
-console.log(SimpleLightbox);
+const galleryMarkup = createMarkup(gallery);
+
+function createMarkup(gallery) {
+  return galleryTpls(gallery);
+}
+
+refs.galleryList.innerHTML = '';
+refs.galleryList.insertAdjacentHTML('beforeend', galleryMarkup);
+
+refs.galleryList.addEventListener('click', onClick);
+
+function onClick(e) {
+  if (e.currentTarget === e.target) return;
+  e.preventDefault();
+}
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+  captionsData: 'alt',
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,19 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
 
+function whileOn(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player.on('timeupdate', throttle(whileOn, 1000));
+
+function onPlay(data) {
+  let savedTime = localStorage.getItem('videoplayer-current-time');
+  if (data.seconds !== savedTime) {
+    player.setCurrentTime(savedTime);
+  }
+}
+
+player.on('play', onPlay);
